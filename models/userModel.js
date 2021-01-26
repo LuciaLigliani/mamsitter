1ce51fe1cd6e9a1a138f09f9226255390f6b9c3a
LuciaLigliani <lucia.ligliani@studenti.unicam.it>
2021-01-26 17:24:36 +0100

    collegamento frontend

--- models/userModel.js
+++ models/userModel.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
+      required: [true, 'Please provide a role'],
       enum: ['famiglia', 'lavoratore', 'admin']
     },
     password: {
