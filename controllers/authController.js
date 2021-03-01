ebea463e49fa36854e29d9445f50d9f96398e22f
LuciaLigliani <lucia.ligliani@studenti.unicam.it>
2021-03-01 16:10:41 +0100

    cookie jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,17 +36,12 @@ exports.login = catchAsync(async (req, res, next) => {
 
 // means that you have to authenticate
 exports.protect = catchAsync(async (req, res, next) => {
-  // getting token and check if it's there
   let token;
-  console.log('ciao');
+
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
   }
-  console.log('hola');
   
-
   if (!token) {
     return next(new AppError('You are not logged in! Please log in to get access.', 401));
   }
