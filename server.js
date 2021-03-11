a08ef20f83af7e8e74d9736aab5693aa9df14eee
LuciaLigliani <lucia.ligliani@studenti.unicam.it>
2021-03-11 15:49:41 +0100

    correzione errori

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE;
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
